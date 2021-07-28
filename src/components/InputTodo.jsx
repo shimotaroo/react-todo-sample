46e022c08b5a89109fe61d735d63fa6507f5b466
Yutaro Shimoda <58982088+shimotaroo@users.noreply.github.com>
2021-07-28 23:29:20 +0900

    TODOの上限値の設定

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -14,12 +14,19 @@ const style = {
 // 親コンポーネントからのproosを受け取る場合は引数にpropsを指定する
 export const InputTodo = (props) => {
   // 親コンポーネントから受け取ったpropsを分割代入する
-  const { todoText, onChange, onClick } = props;
+  const { todoText, onChange, onClick, disabled } = props;
 
   return (
     <div style={style}>
-      <input placeholder="TODOを入力" value={todoText} onChange={onChange} />
-      <button onClick={onClick}>追加</button>
+      <input
+        disabled={disabled}
+        placeholder="TODOを入力"
+        value={todoText}
+        onChange={onChange}
+      />
+      <button disabled={disabled} onClick={onClick}>
+        追加
+      </button>
     </div>
   );
 };
